33651d6980adb9164f2d4897ad0e11ed2b1d0fce
Mark Ogilo <44981320+codescream@users.noreply.github.com>
2019-01-19 07:42:48 -0500

    Add project files.

---
+++ FredQnA/JsonNinja.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieMarvel
+{
+    public class JsonNinja
+    {
+        List<string> names = new List<string>();
+        List<string> vals = new List<string>();
+
+        public List<string> GetVals()
+        {
+            return vals;
+        }
+
+        public JsonNinja(string data)
+        {
+            names.Clear();
+            vals.Clear();
+            data = data.Replace("}", "");
+            data = data.Replace("{", "");
+            int startPos = 0;
+            int endPos = 0;
+            int endPosVal = 0;
+            bool startFound = false;
+            bool endFound = false;
+            bool collection = false;
+            int collectionCounter = 0;
+
+            int k = 0;
+            while(k < data.Length)
+            {
+                char test = data[k];
+                if (data[k] == '"' && startFound == false)
+                {
+                    startFound = true;
+                    startPos = k;
+                }
+                if(data[k] == ':' && endFound == false)
+                {
+                    if (data[k + 1] == '[')
+                    {
+                        collection = true;
+                    }
+                    endPos = k;
+                    endFound = true;
+
+                    string nameCut = data.Substring(startPos, endPos - startPos);
+                    names.Add(nameCut);
+                }
+
+                if (collection == true)
+                {
+                    if (data[k] == '[')
+                        collectionCounter++;
+
+                    if(data[k] == ']')
+                    {
+                        if(collectionCounter == 1)
+                        {
+                            endPosVal = k;
+                            string valCut = data.Substring(endPos + 1, endPosVal - (endPos));
+                            vals.Add(valCut);
+                            startFound = false;
+                            endFound = false;
+                            collectionCounter = 0;
+                            collection = false;
+                        }
+                        else
+                        {
+                            collectionCounter--;
+                        }
+                    }
+                }
+                else if ((data[k] == ',' && data[k + 1] == '"' && startFound == true) || k == data.Length - 1)
+                {
+                    if(k == data.Length - 1)
+                    {
+                        endPosVal = k;
+                        string valCut = data.Substring(endPos + 1, endPosVal - (endPos));
+                        vals.Add(valCut);
+                        startFound = false;
+                        endFound = false;
+                    }
+                    else
+                    {
+                        endPosVal = k;
+                        string valCut = data.Substring(endPos + 1, endPosVal - (endPos + 1));
+                        vals.Add(valCut);
+                        startFound = false;
+                        endFound = false;
+                    }
+                }
+                k++;
+            }
+        } // end constructor
+
+        public List<string> GetNames()
+        {
+            return names;
+        }
+
+        public List<string> GetPosters(string name)
+        {
+            List<string> value = new List<string>();
+            for (int i = 0; i < names.Count; i++)
+            {
+                if (name == names[i])
+                {
+                    if(vals[i] != "null")
+                    {
+                        value.Add(vals[i].Substring(3, vals[i].Length - 4));
+                    }
+                    else
+                    {
+                        value.Add("");
+                    }
+                }
+            }
+            // trims ""
+            return value;
+        }
+
+        public List<string> GetIds(string name)
+        {
+            List<string> value = new List<string>();
+            for (int i = 1; i < names.Count; i++)
+            {
+                if (name == names[i])
+                {
+                    if(vals[i] != "null")
+                        value.Add(vals[i]);
+                }
+            }
+            // trims ""
+            return value;
+        }
+
+        public List<string> GetDetails(string key)
+        {
+            List<string> value = new List<string>();
+            for (int i = 0; i < names.Count; i++)
+            {
+                if (key == names[i])
+                {
+                    if (vals[i] == "nul" || vals[i] == "null" || vals[i] == "")
+                    {
+                        value.Add("null");
+                    }
+                    else
+                    {
+                        value.Add(vals[i].Substring(1, vals[i].Length - 2));
+                    }
+                }
+            }
+            // trims ""
+            return value;
+        }
+    }// end class
+}// end namespace
---
+++ FredQnA/Program.cs
@@ -0,0 +1,109 @@
+﻿using MovieMarvel;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FredQnA
+{
+    class Program
+    {
+        public static HttpClient client = new HttpClient();
+        static string appKey = Environment.GetEnvironmentVariable("Wofram_App_Key", EnvironmentVariableTarget.User);
+        static string wolframText = "";
+      
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a search:");
+            string search = Console.ReadLine();
+            GetAnswer(search).Wait();
+        }
+
+        public static async Task GetAnswer(string search)
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue
+                ("applicationException/json"));
+
+            // grab 20 vids
+            HttpResponseMessage response = await client.GetAsync("https://api.wolframalpha.com/v1/result?i=" + search + "&appid=" + appKey);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string Data = await response.Content.ReadAsStringAsync();
+                wolframText = Data.Split('.')[0];
+                //Console.WriteLine(wolframText);
+                if (wolframText.Length < 15)
+                {
+                    AskQuestion(search).Wait();
+                }
+                else
+                {
+                    Console.WriteLine(wolframText);
+                }
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("please rephrase your question");
+                string newSearch = Console.ReadLine();
+                await GetAnswer(newSearch);
+            }
+        }
+
+        private static async Task AskQuestion(string question)
+        {
+            string newText = "";
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue
+                ("applicationException/json"));
+
+            HttpResponseMessage response = await client.GetAsync("http://api.duckduckgo.com/?q=" + question + "&format=json");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string Data = await response.Content.ReadAsStringAsync();
+                JsonNinja ninja = new JsonNinja(Data);
+                List<string> answer = ninja.GetDetails("\"Abstract\"");
+                List<string> rTopics = ninja.GetDetails("\"RelatedTopics\"");
+                JsonNinja ninji = new JsonNinja(rTopics[0]);
+                List<string> texts = ninji.GetDetails("\"Text\"");
+                //Console.WriteLine("Answer: \n");
+                if (answer[0] != "")
+                {
+                    string addStr = answer[0].Split('.')[0];
+                    wolframText += "\n" + addStr;
+                    Console.WriteLine(addStr);
+                }
+                else
+                {
+                    if (texts.Count == 0)
+                    {
+                        string data = ""; //this is so that if the search field is empty it does not show what was last searched
+                        Console.WriteLine(wolframText);
+                    }
+                    else
+                    {
+                        int count = 1;
+                        wolframText += "\nFound " + texts.Count + " other result(s)";
+                        //Console.WriteLine("Found " + texts.Count + " results");
+                        foreach (string text in texts)
+                        {
+                            newText += count + ": " + text.Split('.')[0].Replace("\\", "") + "\n";
+                            //Console.WriteLine(count + ": " + newText + "\n");
+                            count++;
+                        }
+                        wolframText += "\n" + newText;
+                        Console.WriteLine(wolframText);
+                    }
+                }
+            }
+            else
+            {
+                string Data = ""; //this is so that if the search field is empty it does not show what was last searched
+                Console.WriteLine(Data);
+            }
+        }
+    }
+}
